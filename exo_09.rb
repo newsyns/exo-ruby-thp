6e84b96cbb20de58324689adcd6e411047fd2733
Syns <jpo.drt@gmail.com>
2019-01-07 18:59:38 +0100

    ajout de l'exo 08 et 09

---
+++ exo_09.rb
@@ -0,0 +1,9 @@
+puts "Quel est votre prénom ?"
+print "> "
+first_name = gets.chomp
+
+puts "Quel est votre nom ?"
+print "> "
+last_name = gets.chomp
+
+puts "Bonjour, #{first_name} #{last_name} !"
